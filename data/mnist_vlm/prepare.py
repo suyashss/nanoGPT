d990e4ee4f56c812e0330498252e01dfafc299b9
Suyash Shringarpure <suyashss+github@gmail.com>
2025-01-19 12:42:37 -0800

    Script to prepare mnist text data for nanoGPT modeling

---
+++ data/mnist_vlm/prepare.py
@@ -0,0 +1,74 @@
+"""
+Prepare the mnist vlm dataset for character-level language modeling.
+We will just map characters to ints.
+Will save train.bin, val.bin, test.bin containing the token ids, and meta.pkl containing the
+encoder and decoder and some other related info.
+"""
+import os
+import pickle
+import numpy as np
+
+# Load the mnist vlm processed files from the output of "format_vlm_input.py"
+train_file_path = os.path.join(os.path.dirname(__file__), 'train.txt')
+valid_file_path = os.path.join(os.path.dirname(__file__), 'valid.txt')
+test_file_path = os.path.join(os.path.dirname(__file__), 'test.txt')
+
+data = ""
+split_data = []
+file_path_list = [train_file_path,valid_file_path,test_file_path]
+for input_file_path in file_path_list:
+    curr_data = ""
+    with open(input_file_path, 'r') as f:
+        for line in f:
+            curr_data += line.strip()
+    data += curr_data
+    split_data.append(curr_data)
+print(f"length of dataset in characters: {len(data):,}")
+
+# get all the unique characters that occur in this text
+chars = sorted(list(set(data)))
+vocab_size = len(chars)
+print("all the unique characters:", ''.join(chars))
+print(f"vocab size: {vocab_size:,}")
+
+# create a mapping from characters to integers
+stoi = { ch:i for i,ch in enumerate(chars) }
+itos = { i:ch for i,ch in enumerate(chars) }
+def encode(s):
+    return [stoi[c] for c in s] # encoder: take a string, output a list of integers
+def decode(l):
+    return ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+train_data, val_data, test_data = split_data
+
+# encode all data to integer ids
+train_ids = encode(train_data)
+val_ids = encode(val_data)
+test_ids = encode(test_data)
+print(f"train has {len(train_ids):,} tokens")
+print(f"val has {len(val_ids):,} tokens")
+print(f"test has {len(test_ids):,} tokens")
+
+# export to bin files
+train_ids = np.array(train_ids, dtype=np.uint16)
+val_ids = np.array(val_ids, dtype=np.uint16)
+test_ids = np.array(test_ids, dtype=np.uint16)
+train_ids.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
+val_ids.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
+test_ids.tofile(os.path.join(os.path.dirname(__file__), 'test.bin'))
+
+# save the meta information as well, to help us encode/decode later
+meta = {
+    'vocab_size': vocab_size,
+    'itos': itos,
+    'stoi': stoi,
+}
+with open(os.path.join(os.path.dirname(__file__), 'meta.pkl'), 'wb') as f:
+    pickle.dump(meta, f)
+
+#length of dataset in characters: TO UPDATE
+#all the unique characters:  TO UPDATE
+#vocab size: TO UPDATE
+#train has TO UPDATE tokens
+#val has TO UPDATE tokens
+#test has TO UPDATE tokens
