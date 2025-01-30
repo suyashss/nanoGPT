8159342b11bcc996607435699d6c9bbd1d7ebb20
Suyash Shringarpure <suyashss+github@gmail.com>
2025-01-30 08:10:26 -0800

    Comment out default wandb run_name

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # wandb logging
 wandb_log = False # disabled by default
 wandb_project = 'owt'
-wandb_run_name = 'gpt2' # 'run' + str(time.time())
+#wandb_run_name = 'gpt2' # 'run' + str(time.time())
 # data
 dataset = 'openwebtext'
 gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
